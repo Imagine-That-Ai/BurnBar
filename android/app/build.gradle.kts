cb6aa2156b4eb29a0379839e7432aa5637f6e92e
Alberto Nunez <ajnunezg@users.noreply.github.com>
2026-06-03 11:08:17 -0500

    chore(release): cut latest mobile store builds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.openburnbar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.0.16"
+        versionCode = 27
+        versionName = "1.0.17"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         // App Check: when this build is meant for Firebase App Distribution
