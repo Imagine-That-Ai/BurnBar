7870072942ed69f1e46a2715a37d86e980fe77ab
Alberto Nunez <125839313+Ajnunezg@users.noreply.github.com>
2026-06-12 18:34:00 -0500

    ci: fix launch hardening gates

--- packages/data-domains/gen/domains.ts
+++ packages/data-domains/gen/domains.ts
@@ -518,6 +518,7 @@ export const DATA_DOMAINS: readonly DataDomain[] = [
       "cloud_vault_state",
       "cloud_vault_key_wrappers",
       "cloud_vault_rotation_jobs",
+      "cloud_vault_rotation_requirements",
       "escrow_devices",
       "escrow_public_keys",
       "signal_identity_public_keys",
