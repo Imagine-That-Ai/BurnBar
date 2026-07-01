2dfbfccda4e9edc535037cf8cc4bda879112b964
Alberto <125839313+Ajnunezg@users.noreply.github.com>
2026-07-01 01:20:44 -0500

    fix: harden mac release app heartbeat for v1.0.19 (#1121)

--- OpenBurnBarDaemon/Sources/OpenBurnBarDaemon/OpenBurnBarDaemonConfiguration.swift
+++ OpenBurnBarDaemon/Sources/OpenBurnBarDaemon/OpenBurnBarDaemonConfiguration.swift
@@ -89,7 +89,7 @@ public enum BurnBarDaemonPaths {
 }
 
 public enum BurnBarDaemonVersion {
-    public static let current = "1.0.18"
+    public static let current = "1.0.19"
 }
 
 public struct BurnBarGatewayConfiguration: Codable, Hashable, Sendable {
