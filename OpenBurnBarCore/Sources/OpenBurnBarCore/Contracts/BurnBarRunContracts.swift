726f05114fa3a8b5b68294b4df1f17ee802a2185
Imagine <dewclaw@mac.attlocal.net>
2026-07-05 20:31:20 -0500

    fix(windows): share run phase contract

--- OpenBurnBarCore/Sources/OpenBurnBarCore/Contracts/BurnBarRunContracts.swift
+++ OpenBurnBarCore/Sources/OpenBurnBarCore/Contracts/BurnBarRunContracts.swift
@@ -1,17 +1,5 @@
 import Foundation
 
-public enum BurnBarRunPhase: String, Codable, CaseIterable, Hashable, Sendable {
-    case idle
-    case planning
-    case awaitingApproval = "awaiting_approval"
-    case executingTool = "executing_tool"
-    case waitingOnCompanion = "waiting_on_companion"
-    case modelStreaming = "model_streaming"
-    case completed
-    case failed
-    case cancelled
-}
-
 public struct BurnBarRunStateSnapshot: Codable, Hashable, Sendable {
     public let runID: BurnBarRunID
     public let clientID: BurnBarClientID
